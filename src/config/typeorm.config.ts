3cc96eb79968cd618b5859a6ad8200bac3bdeab0
a-rudolph <55548329+a-rudolph@users.noreply.github.com>
2020-09-29 21:59:33 -0400

    chore: cleanup configs for v0.2

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,4 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { password } from '../../.env';
 import * as config from 'config';
 
 const dbConfig = config.get('db');
@@ -9,7 +8,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: process.env.RDS_PORT || dbConfig.port,
   username: process.env.RDS_USERNAME || dbConfig.username,
-  password: process.env.RDS_PASSWORD || password || dbConfig.password,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
